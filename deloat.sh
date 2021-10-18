7c5b1f5fc9c71491ddba4234fba405a6f1daf0a6
paritosh9199 <paritoshsrivastava9199@gmail.com>
2021-10-19 00:04:16 +0530

    Initial commit

---
+++ deloat.sh
@@ -0,0 +1,75 @@
+adb shell pm uninstall -k --user 0 com.android.bips 
+adb shell pm uninstall -k --user 0 com.android.bookmarkprovider
+adb shell pm uninstall -k --user 0 com.android.browser
+adb shell pm uninstall -k --user 0 com.android.mms 
+adb shell pm uninstall -k --user 0 com.android.mms.service 
+adb shell pm uninstall -k --user 0 com.android.printspooler
+adb shell pm uninstall -k --user 0 com.android.statementservice 
+adb shell pm uninstall -k --user 0 com.android.thememanager
+adb shell pm uninstall -k --user 0 com.android.thememanager.module
+
+# adb shell pm uninstall -k --user 0 com.android.wallpaper.livepicker
+# adb shell pm uninstall -k --user 0 com.android.wallpaperbackup
+# adb shell pm uninstall -k --user 0 com.android.wallpapercropper 
+
+adb shell pm uninstall -k --user 0 cn.wps.xiaomi.abroad.lite
+adb shell pm uninstall -k --user 0 com.caf.fmradio
+adb shell pm uninstall -k --user 0 com.facebook.appmanager
+adb shell pm uninstall -k --user 0 com.facebook.katana
+adb shell pm uninstall -k --user 0 com.facebook.services
+adb shell pm uninstall -k --user 0 com.facebook.system
+adb shell pm uninstall -k --user 0 com.mi.android.globalFileexplorer
+adb shell pm uninstall -k --user 0 com.mi.android.globalminusscreen
+
+adb shell pm uninstall -k --user 0 com.miui.analytics
+adb shell pm uninstall -k --user 0 com.miui.android.fashiongallery
+adb shell pm uninstall -k --user 0 com.miui.extraphoto
+adb shell pm uninstall -k --user 0 com.miui.fm
+adb shell pm uninstall -k --user 0 com.miui.gallery
+adb shell pm uninstall -k --user 0 com.miui.hybrid
+adb shell pm uninstall -k --user 0 com.miui.hybrid.accessory
+adb shell pm uninstall -k --user 0 com.miui.micloudsync
+adb shell pm uninstall -k --user 0 com.miui.miservice
+adb shell pm uninstall -k --user 0 com.miui.mishare.connectivity
+adb shell pm uninstall -k --user 0 com.miui.miwallpaper
+adb shell pm uninstall -k --user 0 com.miui.msa.global
+adb shell pm uninstall -k --user 0 com.miui.phrase
+adb shell pm uninstall -k --user 0 com.miui.player
+adb shell pm uninstall -k --user 0 com.miui.qr
+
+adb shell pm uninstall -k --user 0 com.miui.touchassistant
+adb shell pm uninstall -k --user 0 com.miui.weather2
+adb shell pm uninstall -k --user 0 com.miui.yellowpage
+
+adb shell pm uninstall -k --user 0 com.xiaomi.account
+adb shell pm uninstall -k --user 0 com.xiaomi.calendar
+adb shell pm uninstall -k --user 0 com.xiaomi.joyose
+adb shell pm uninstall -k --user 0 com.xiaomi.location.fused
+adb shell pm uninstall -k --user 0 com.xiaomi.mi_connect_service
+adb shell pm uninstall -k --user 0 com.xiaomi.micloud.sdk
+adb shell pm uninstall -k --user 0 com.xiaomi.midrop
+adb shell pm uninstall -k --user 0 com.xiaomi.miplay_client
+adb shell pm uninstall -k --user 0 com.xiaomi.payment
+adb shell pm uninstall -k --user 0 com.xiaomi.scanner
+
+
+adb shell pm uninstall -k --user 0 com.miui.calculator
+adb shell pm uninstall -k --user 0 com.android.deskclock
+adb shell pm uninstall -k --user 0 com.android.providers.downloads
+adb shell pm uninstall -k --user 0 com.android.providers.downloads.ui
+
+adb shell pm uninstall -k --user 0 com.android.soundrecorder
+# com.android.calendar
+
+# adb shell pm uninstall -k --user 0 com.xiaomi.misettings
+
+# Dont debloat these apps as it causes bootloop after rebooting the phone
+# adb shell pm uninstall -k --user 0 com.xiaomi.mipicks
+# adb shell pm uninstall -k --user 0 com.mipay.wallet.id
+# adb shell pm uninstall -k --user 0 com.mipay.wallet.in
+# adb shell pm uninstall -k --user 0 com.miui.securityadd
+# adb shell pm uninstall -k --user 0 com.miui.securitycore
+# adb shell pm uninstall -k --user 0 com.miui.securitycenter
+# com.miui.virtualsim
+# com.mi.globalTrendNews
+# com.micredit.in
